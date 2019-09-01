d14382c745a843aa186b30709607d85aab44888d
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2019-09-01 16:59:53 -0500

    proofreading

--- pages/_partials/About.js
+++ pages/_partials/About.js
@@ -9,11 +9,10 @@ export default ()=>{
           <span className="subtitle text-uppercase">About Myself</span>
           <h2 className="font-weight-bold text-uppercase title-2">Creating and Building Creative Idea.</h2>
             <p>
-              I'm currently a Software Engineer, work with BlueTide Web Consulting at Ciudad del Saber. 
-              Trying to think new ideas that can improve and optimize some process in the industry, 
-              It's funny because not every ideas are good but some are. In my free time I spend my time on youtube watching Platzi News or Netflix series. 
-              I would like to take some trips to explore new cultures, I'm interested in Asian Culture. 
-              I can't leave behind Videogames, since childhood I really like to play on my PC videogames. 
+              I'm a Software Engineer, currently working with BlueTide Web Consulting at Ciudad del Saber. 
+              Trying to think new ideas that can improve and optimize some process in the industry, sometimes i get some funny ideas that may work once implemented, 
+              but some of them are just for fun and to improve myself. In my free time I spend my time on youtube watching Platzi News or Netflix series. 
+              I would like to take some trips to explore new cultures, I'm interested in Asian Culture. I can't leave behind Videogames, since childhood I really like to play on my PC videogames. 
               Yep, and I still have to discover more things.
             </p>
         </div>
